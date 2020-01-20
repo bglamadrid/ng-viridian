4ad6d4c0ba3cbdb8cc7869623f3f25dbadfcec42
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-01-20 20:49:02 -0300

    [m] better doc

--- src/models/Descriptable.ts
+++ src/models/Descriptable.ts
@@ -2,6 +2,7 @@ import { AbstractEntity } from './AbstractEntity';
 
 /**
  * An element that can be described, and for instance, has been given a name.
+ * Sometimes its description may just be its name.
  */
 export interface Descriptable
   extends AbstractEntity {
