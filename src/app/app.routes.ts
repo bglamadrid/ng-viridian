bfca91ff6c23a2e98523300437499804b410634e
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-02-08 10:15:03 -0300

    [f] route pointing to questions/answers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './routed_components/landing/landing.component';
 import { DeviceCatalogComponent } from './routed_components/device-catalog/device-catalog.component';
+import { QuestionsAnswersComponent } from './routed_components/questions-answers/questions-answers.component';
 
 export const BASE_ROUTE = '';
 
 export const APP_ROUTES: Routes = [
   { path: 'inicio', component: LandingComponent, canActivate: [] },
-  { path: 'foro', component: DeviceCatalogComponent, canActivate: [] },
+  { path: 'foro', component: QuestionsAnswersComponent, canActivate: [] },
   { path: 'equipos', component: DeviceCatalogComponent, canActivate: [] },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
