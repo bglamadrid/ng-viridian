fb088736dbff075691ec9eb09a253c5c79885e17
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-02-14 22:26:51 -0300

    landing should have 'latest questions' table grid

--- src/app/routed_components/landing/landing.component.ts
+++ src/app/routed_components/landing/landing.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Observable, Subscription, of } from 'rxjs';
+import { Question } from 'src/models/entities/Question';
+import { QuestionsAnswersService } from '../questions-answers/questions-answers.service';
+import { QuestionsGridComponent } from '../questions-answers/questions-grid/questions-grid.component';
 
 @Component({
+  providers: [ QuestionsAnswersService ],
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: [
@@ -8,5 +13,40 @@ import { Component } from '@angular/core';
     './landing.component.sass'
   ]
 })
-export class LandingComponent {
+export class LandingComponent
+  implements OnInit {
+
+  // TODO
+
+  @ViewChild('questionsGrid', { static: true }) public grid: QuestionsGridComponent;
+  public latestQuestions$: Observable<Question[]>;
+  public latestQuestionsLoad: Subscription;
+
+  public get loadingLatestQuestions(): boolean {
+    return this.latestQuestionsLoad ? this.latestQuestionsLoad.closed : false;
+  }
+
+  constructor(
+
+    /* UNCOMMENT WHEN item collection size/count is implemented
+      protected qSvc: QuestionsAnswersService
+    */
+  ) {
+    // DELETE this and the next 3 lines
+    // WHEN item collection size/count is implemented
+    this.latestQuestions$ = of([]);
+    this.latestQuestionsLoad = this.latestQuestions$.subscribe();
+  }
+
+  ngOnInit() {
+
+    /* UNCOMMENT WHEN item collection size/count is implemented
+
+      this.latestQuestions$ = this.qSvc.questionsSource.asObservable();
+      this.latestQuestionsLoad = this.latestQuestions$.subscribe();
+      this.qSvc.reloadQuestions();
+
+    */
+  }
+
 }
