b13a91c61d9ed961160fe7a6d1422c85adb96e7e
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-02-22 17:29:12 -0300

    remove unused dialogs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,22 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataActionsComponent } from './components/data-actions/data-actions.component';
 import { MatProgressSpinnerCenteredComponent } from './components/mat-spinner-centered/mat-spinner-centered.component';
-import { PersonFormComponent } from './components/person-form/person-form.component';
 import { ConfirmationDialogComponent } from './dialogs/confirmation/confirmation.dialog.component';
-import { UserProfileDialogComponent } from './dialogs/user-profile/user-profile.dialog.component';
 import { MaterialModule } from './material/shared.material.module';
 
 @NgModule({
   declarations: [
     MatProgressSpinnerCenteredComponent,
     DataActionsComponent,
-    ConfirmationDialogComponent,
-    PersonFormComponent,
-    UserProfileDialogComponent
+    ConfirmationDialogComponent
   ],
   entryComponents: [
-    ConfirmationDialogComponent,
-    UserProfileDialogComponent
+    ConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -40,9 +35,7 @@ import { MaterialModule } from './material/shared.material.module';
 
     DataActionsComponent,
     MatProgressSpinnerCenteredComponent,
-    PersonFormComponent,
-    ConfirmationDialogComponent,
-    UserProfileDialogComponent
+    ConfirmationDialogComponent
   ]
 })
 export class SharedModule { }
