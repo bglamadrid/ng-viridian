7a0d1555dc3fbcff291c28652931b91705dc47f6
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-02-25 11:41:14 -0300

    add new labels, slight order change

--- src/app/shared/i18/es/labels.ts
+++ src/app/shared/i18/es/labels.ts
@@ -16,15 +16,17 @@ export const LBL_VALUE = 'Valor';
 export const LBL_ADD_DEVICE = 'Nuevo dispositivo';
 export const LBL_DISCONNECT = 'Desconectar';
 export const LBL_UPDATE = 'Actualizar';
-export const LBL_REPLY_TO_THIS_QUESTION = 'Responder a esta pregunta';
+export const LBL_ASK = 'Preguntar';
 export const LBL_REPLY = 'Responder';
+export const LBL_ASK_A_QUESTION = 'Hacer una pregunta';
+export const LBL_REPLY_TO_THIS_QUESTION = 'Responder a esta pregunta';
 
 export const LBL_DATE_CREATED = 'Creada en';
 export const LBL_ANSWERS_COUNT = 'Respuestas';
+
 export const LBL_NO_FILTER = 'No filtrar';
 export const LBL_NO_PHOTOS = 'No hay fotos asociadas';
 export const LBL_NO_DESCRIPTION = '(Sin descripción)';
-
 
 export const LBL_TOGGLE_SIDEMENU = 'Alternar menú lateral';
 export const LBL_EDIT_PROFILE = 'Editar perfil';
