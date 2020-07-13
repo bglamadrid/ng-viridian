d90065e1035e8807b48070651bc265342cf484c5
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-07-13 10:56:10 -0400

    updates application & package name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/app-forein'),
+      dir: require('path').join(__dirname, './coverage/ng-forein'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-forein'`, () => {
+  it(`should have as title 'ng-forein'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app-forein');
+    expect(app.title).toEqual('ng-forein');
   });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('app-forein app is running!');
+  //   expect(compiled.querySelector('.content span').textContent).toContain('ng-forein app is running!');
   // });
 });
