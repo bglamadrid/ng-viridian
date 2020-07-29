77cd15dc92be7d7a294d70472c15dcbbeba5f746
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-07-29 19:16:24 -0400

    cleaned up landing files

--- src/app/routes/landing/landing.component.ts
+++ src/app/routes/landing/landing.component.ts
@@ -1,49 +1,13 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { Observable, Subscription, of } from 'rxjs';
-import { ForumThread } from 'src/data/models/entities/ForumThread';
+import { Component } from '@angular/core';
 import { ForumService } from '../forum/forum.service';
-import { ForumThreadListComponent } from '../forum/thread-list/forum-thread-list.component';
 
 @Component({
-  providers: [ ForumService ],
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: [ './landing.component.css' ]
 })
-export class LandingComponent
-  implements OnInit {
+export class LandingComponent {
 
-  // TODO
-
-  @ViewChild('questionsGrid', { static: true }) public grid: ForumThreadListComponent;
-  public latestQuestions$: Observable<ForumThread[]>;
-  public latestQuestionsLoad: Subscription;
-
-  public get loadingLatestQuestions(): boolean {
-    return this.latestQuestionsLoad ? this.latestQuestionsLoad.closed : false;
+  constructor() {
   }
-
-  constructor(
-
-    /* UNCOMMENT WHEN item collection size/count is implemented
-      protected qSvc: QuestionsAnswersService
-    */
-  ) {
-    // DELETE this and the next 3 lines
-    // WHEN item collection size/count is implemented
-    this.latestQuestions$ = of([]);
-    this.latestQuestionsLoad = this.latestQuestions$.subscribe();
-  }
-
-  ngOnInit() {
-
-    /* UNCOMMENT WHEN item collection size/count is implemented
-
-      this.latestQuestions$ = this.qSvc.questionsSource.asObservable();
-      this.latestQuestionsLoad = this.latestQuestions$.subscribe();
-      this.qSvc.reloadQuestions();
-
-    */
-  }
-
 }
