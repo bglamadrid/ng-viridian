e98fcbc0c998371735ad4d72bf8fa53c150ceca3
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-13 18:48:52 -0400

    added courses to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SelectivePreloadingStrategyService } from 'src/app/selective-preloading-strategy.service';
-import { LandingComponent } from './routes/landing/landing.component';
-import { ForumComponent } from './routes/qa-forum/forum.component';
 import { DevicesComponent } from './routes/devices-catalog/devices.component';
+import { LandingComponent } from './routes/landing/landing.component';
+import { OnlineCoursesComponent } from './routes/online-courses/online-courses.component';
+import { ForumComponent } from './routes/qa-forum/forum.component';
 
 export const APP_ROUTES: Routes = [
   { path: 'inicio', component: LandingComponent, canActivate: [] },
   { path: 'foro', component: ForumComponent, canActivate: [] },
   { path: 'equipos', component: DevicesComponent, canActivate: [] },
+  { path: 'cursos', component: OnlineCoursesComponent, canActivate: [] },
   { path: '', redirectTo: 'inicio', pathMatch: 'full' }
 ];
 
