dcaf66b2135f96565e81136120cb03c358f67fdb
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-13 19:30:29 -0400

    orders routes, new extending interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,43 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 import { SelectivePreloadingStrategyService } from 'src/app/selective-preloading-strategy.service';
 import { DevicesComponent } from './routes/devices-catalog/devices.component';
 import { LandingComponent } from './routes/landing/landing.component';
 import { OnlineCoursesComponent } from './routes/online-courses/online-courses.component';
 import { ForumComponent } from './routes/qa-forum/forum.component';
 
-export const APP_ROUTES: Routes = [
-  { path: 'inicio', component: LandingComponent, canActivate: [] },
-  { path: 'foro', component: ForumComponent, canActivate: [] },
-  { path: 'equipos', component: DevicesComponent, canActivate: [] },
-  { path: 'cursos', component: OnlineCoursesComponent, canActivate: [] },
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+export interface AppRoute extends Route {
+  data?: { matIcon: string, title: string };
+}
+
+export const APP_ROUTES: AppRoute[] = [
+  {
+    path: 'inicio',
+    component: LandingComponent,
+    data: { matIcon: 'home', title: 'Inicio' },
+    canActivate: []
+  },
+  {
+    path: 'foro',
+    component: ForumComponent,
+    data: { matIcon: 'question_answer', title: 'Preguntas y Respuestas' },
+    canActivate: []
+  },
+  {
+    path: 'equipos',
+    component: DevicesComponent,
+    data: { matIcon: 'devices', title: 'Equipos' },
+    canActivate: []
+  },
+  {
+    path: 'cursos',
+    component: OnlineCoursesComponent,
+    data: { matIcon: 'school', title: 'Clases TIC 2.0' },
+    canActivate: []
+  },
+  {
+    path: '', redirectTo: 'inicio', pathMatch: 'full'
+  }
 ];
 
 @NgModule({
