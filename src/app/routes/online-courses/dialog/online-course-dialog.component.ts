525d73f76c6e4ca8a754e3a9bc644be4190edf9a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-13 21:52:15 -0400

    sanitize youtube url with new pipe

--- src/app/routes/online-courses/dialog/online-course-dialog.component.ts
+++ src/app/routes/online-courses/dialog/online-course-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OnlineCourse } from 'src/data/models/entities/OnlineCourse';
 import { OnlineCourseDialogData } from './OnlineCourseDialogData';
+import { SafeHtml } from '@angular/platform-browser';
+import { LBL_NO_PHOTOS } from 'src/text/es/labels';
 
 @Component({
   selector: 'app-online-course-dialog',
@@ -12,10 +14,13 @@ export class OnlineCourseDialogComponent {
 
   public course: OnlineCourse;
 
+  public youtubeURL: string;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) data: OnlineCourseDialogData
   ) {
     this.course = data.onlineCourse;
+    this.youtubeURL =  'https://www.youtube.com/embed/' + data.onlineCourse.youtubeVideoId;
   }
 
 }
