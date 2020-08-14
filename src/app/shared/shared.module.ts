55eeccd65a9331f0b6eefaa187a9206ef76e4e43
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-13 21:47:14 -0400

    adds new 'safe' pipe for urls
    https://stackoverflow.com/questions/38037760

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatProgressSpinnerCenteredComponent } from './mat-spinner-centered/mat-spinner-centered.component';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation.dialog.component';
 import { AngularMaterialModule } from './angular-material.module';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
     MatProgressSpinnerCenteredComponent,
-    ConfirmationDialogComponent
+    ConfirmationDialogComponent,
+    SafePipe
   ],
   entryComponents: [
     ConfirmationDialogComponent
@@ -17,7 +19,8 @@ import { AngularMaterialModule } from './angular-material.module';
   exports: [
     AngularMaterialModule,
     MatProgressSpinnerCenteredComponent,
-    ConfirmationDialogComponent
+    ConfirmationDialogComponent,
+    SafePipe
   ]
 })
 export class SharedModule { }
